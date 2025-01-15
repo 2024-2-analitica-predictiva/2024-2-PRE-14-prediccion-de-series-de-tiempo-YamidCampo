25ae3ac140213f1961f2bd932b770f3c7ecbbba9
YamidCampo <69608801+YamidCampo@users.noreply.github.com>
2025-01-14 19:44:38 -0500

    tarea completada

--- homework/functions.py
+++ homework/functions.py
@@ -6,7 +6,8 @@ import pandas as pd  #  type: ignore
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from statsmodels.tsa.stattools import acf, pacf  #  type: ignore
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf  # 
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf  #  type: ignore
+
 
 def load_data():
     df = pd.read_csv("../files/input/sutter.csv")
@@ -42,7 +43,7 @@ def plot_time_series(df, yt_col="yt_true"):
     plt.xticks(fontsize=8)
     plt.legend()
     plt.show()
-    
+
 def acf_pacf_plots(z):
     """Correlation plot."""
 
@@ -68,10 +69,26 @@ def acf_pacf_plots(z):
     plot_pacf(z, lags=24, ax=plt.gca(), color="k")
     format_plot()
     plt.show()
-    
+
 def add_linear_trend_component(df):
     """Add linear trend component to the dataframe."""
     df = df.assign(trend=list(range(len(df))))
+    return df
+
+def add_sin_cos_components(df):
+
+    df = df.assign(sin_12m=np.sin(2 * np.pi * df.month / 12))
+    df = df.assign(cos_12m=np.cos(2 * np.pi * df.month / 12))
+
+    df = df.assign(sin_6m=np.sin(2 * np.pi * df.month / 6))
+    df = df.assign(cos_6m=np.cos(2 * np.pi * df.month / 6))
+
+    df = df.assign(sin_4m=np.sin(2 * np.pi * df.month / 4))
+    df = df.assign(cos_4m=np.cos(2 * np.pi * df.month / 4))
+
+    df = df.assign(sin_3m=np.sin(2 * np.pi * df.month / 3))
+    df = df.assign(cos_3m=np.cos(2 * np.pi * df.month / 3))
+
     return df
 
 def add_month_component(df):
@@ -143,7 +160,7 @@ def save_forecasts(df):
         forecasts["yt_true"] = df["yt_true"].values
 
     forecasts.to_csv("../files/output/forecasts.csv", index=True)
-    
+
     
 def save_metrics(metrics):
     """Save metrics to a csv file."""
@@ -158,3 +175,47 @@ def save_metrics(metrics):
         metrics = saved_metrics
 
     metrics.to_csv("../files/output/metrics.csv", index=False)
+
+def compute_evaluation_metrics(df, y_true_column="yt_true"):
+    """Compute metrics for train and test sets"""
+
+    cols = [col for col in df.columns if "yt_pred" in col]
+
+    y_train_true = df[y_true_column].iloc[:-24]
+    y_test_true = df[y_true_column].iloc[-24:]
+
+    results = {}
+    results["Metrics"] = ["MSE Train", "MSE Test", "MAE Train", "MAE Test"]
+
+    for col in cols:
+
+        y_train_pred = df[col].iloc[:-24]
+        y_test_pred = df[col].iloc[-24:]
+
+        metrics = [
+            mean_squared_error(y_train_true, y_train_pred),
+            mean_squared_error(y_test_true, y_test_pred),
+            mean_absolute_error(y_train_true, y_train_pred),
+            mean_absolute_error(y_test_true, y_test_pred),
+        ]
+
+        results[col] = metrics
+
+    results = pd.DataFrame(results)
+
+    results = results.round(2)
+
+    return results
+
+def make_lagged_ts(df, p_max, y_column, fmt="lagged_{}m"):
+    for i in range(1, p_max + 1):
+        df[fmt.format(i)] = df[y_column].shift(i)
+    return df
+
+def make_yt_true_scaled(df, scaler):
+    df["yt_true_scaled"] = scaler.fit_transform(df[["yt_true"]])
+    return df
+
+def remove_trend_and_cycle(df, yt_true_name="yt_true"):
+    df[yt_true_name + "_d1d12"] = df[yt_true_name].diff(1).diff(12)
+    return df
